a5e0028a8fe1efb00024a5c92cbc7690f6bdad8e
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-25 18:46:35 +0530

    changes in login state

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,33 +25,42 @@ const Header = () => {
       <AppBar position="sticky">
         <Toolbar>
           <Typography variant="h4">My Blog App</Typography>
-          <Box display={"flex"} marginLeft="auto" marginRight={"auto"}>
-            <Tabs
-              textColor="inherit"
-              value={value}
-              onChange={(e, val) => setValue(val)}
-            >
-              <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
-              <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs" />
-              {/* <Tab label="Create Blog" LinkComponent={Link} to="/create-blog" /> */}
-            </Tabs>
-          </Box>
+          {isLogin && (
+            <Box display={"flex"} marginLeft="auto" marginRight={"auto"}>
+              <Tabs
+                textColor="inherit"
+                value={value}
+                onChange={(e, val) => setValue(val)}
+              >
+                <Tab label="Blogs" LinkComponent={Link} to="/blogs" />
+                <Tab label="My Blogs" LinkComponent={Link} to="/my-blogs" />
+                {/* <Tab label="Create Blog" LinkComponent={Link} to="/create-blog" /> */}
+              </Tabs>
+            </Box>
+          )}
           <Box display={"flex"} marginLeft="auto">
-            <Button
-              sx={{ margin: 1, color: "white" }}
-              LinkComponent={Link}
-              to="/login"
-            >
-              Login
-            </Button>
-            <Button
-              sx={{ margin: 1, color: "white" }}
-              LinkComponent={Link}
-              to="/register"
-            >
-              Register
-            </Button>
-            <Button sx={{ margin: 1, color: "white" }}>Logout</Button>
+            {!isLogin && (
+              <>
+                {" "}
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/login"
+                >
+                  Login
+                </Button>
+                <Button
+                  sx={{ margin: 1, color: "white" }}
+                  LinkComponent={Link}
+                  to="/register"
+                >
+                  Register
+                </Button>
+              </>
+            )}
+            {isLogin && (
+              <Button sx={{ margin: 1, color: "white" }}>Logout</Button>
+            )}
           </Box>
         </Toolbar>
       </AppBar>
