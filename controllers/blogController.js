23cc9d985854212277c76a556795b946e8035851
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-25 17:48:27 +0530

    commit

--- controllers/blogController.js
+++ controllers/blogController.js
@@ -146,27 +146,27 @@ exports.deleteBlogController = async (req, res) => {
 };
 
 //GET USER BLOG
-// exports.userBlogController = async (req, res) => {
-//     try {
-//         const userBlog = await userModel.findById(req.params.id).populate("blogs");
+exports.userBlogController = async (req, res) => {
+    try {
+        const userBlog = await userModel.findById(req.params.id).populate("blogs");
 
-//         if (!userBlog) {
-//             return res.status(404).send({
-//                 success: false,
-//                 message: "blogs not found with this id",
-//             });
-//         }
-//         return res.status(200).send({
-//             success: true,
-//             message: "user blogs",
-//             userBlog,
-//         });
-//     } catch (error) {
-//         console.log(error);
-//         return res.status(400).send({
-//             success: false,
-//             message: "error in user blog",
-//             error,
-//         });
-//     }
-// };
+        if (!userBlog) {
+            return res.status(404).send({
+                success: false,
+                message: "blogs not found with this id",
+            });
+        }
+        return res.status(200).send({
+            success: true,
+            message: "user blogs",
+            userBlog,
+        });
+    } catch (error) {
+        console.log(error);
+        return res.status(400).send({
+            success: false,
+            message: "error in user blog",
+            error,
+        });
+    }
+};
