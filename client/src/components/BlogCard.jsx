eb7868f84ea390367f002695d88af99038a889e8
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-26 22:53:54 +0530

    commit

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -1,96 +1,3 @@
-// import * as React from "react";
-
-// import { useNavigate } from "react-router-dom";
-// import toast from "react-hot-toast";
-// import axios from "axios";
-
-// export default function BlogCard({
-//   title,
-//   description,
-//   image,
-//   username,
-//   time,
-//   id,
-//   isUser,
-// }) {
-//   const navigate = useNavigate();
-//   const formattedTime = new Date(time).toLocaleString();
-//   const handleEdit = () => {
-//     navigate(`/blog-details/${id}`);
-//     // toast.success("Blog Edited");
-//   };
-
-//   const handleDelete = async () => {
-//     try {
-//       const { data } = await axios.delete(`/api/v1/blog/delete-blog/${id}`);
-//       if (data?.success) {
-//         toast.success("Blog Deleted");
-//         window.location.reload();
-//       }
-//     } catch (error) {
-//       console.log(error);
-//     }
-//   };
-
-//   return (
-//     <div class="card" style={{ width: "18rem" }}>
-//       <img src={image} class="card-img-top" alt="blog-image" />
-//       <div class="card-body">
-//         {isUser && (
-//           <div>
-//             <svg
-//               onClick={handleEdit}
-//               xmlns="http://www.w3.org/2000/svg"
-//               width="24"
-//               height="24"
-//               viewBox="0 0 24 24"
-//               fill="none"
-//               stroke="currentColor"
-//               stroke-width="2"
-//               stroke-linecap="round"
-//               stroke-linejoin="round"
-//               class="lucide lucide-pencil"
-//             >
-//               <path d="M17 3a2.85 2.83 0 1 1 4 4L7.5 20.5 2 22l1.5-5.5Z" />
-//               <path d="m15 5 4 4" />
-//             </svg>
-
-//             <svg
-//               xmlns="http://www.w3.org/2000/svg"
-//               width="24"
-//               height="24"
-//               viewBox="0 0 24 24"
-//               fill="none"
-//               stroke="currentColor"
-//               stroke-width="2"
-//               stroke-linecap="round"
-//               stroke-linejoin="round"
-//               class="lucide lucide-trash-2"
-//               onClick={handleDelete}
-//             >
-//               <path d="M3 6h18" />
-//               <path d="M19 6v14c0 1-1 2-2 2H7c-1 0-2-1-2-2V6" />
-//               <path d="M8 6V4c0-1 1-2 2-2h4c1 0 2 1 2 2v2" />
-//               <line x1="10" x2="10" y1="11" y2="17" />
-//               <line x1="14" x2="14" y1="11" y2="17" />
-//             </svg>
-//             <p class="card-text">
-//               <small>
-//                 By: {username} at {formattedTime}
-//               </small>
-//             </p>
-//           </div>
-//         )}
-//         <h5 class="card-title">{title}</h5>
-//         <p class="card-text">{description}</p>
-
-//         {/* <a href="#" class="btn btn-primary">
-//           Go somewhere
-//         </a> */}
-//       </div>
-//     </div>
-//   );
-// }
 import * as React from "react";
 import Card from "@mui/material/Card";
 import CardHeader from "@mui/material/CardHeader";
@@ -103,7 +10,7 @@ import ModeEditIcon from "@mui/icons-material/ModeEdit";
 import DeleteIcon from "@mui/icons-material/Delete";
 import { Box, IconButton } from "@mui/material";
 import { useNavigate } from "react-router-dom";
-import toast from "react-hot-toast";
+// import toast from "react-hot-toast";
 import axios from "axios";
 
 export default function BlogCard({
