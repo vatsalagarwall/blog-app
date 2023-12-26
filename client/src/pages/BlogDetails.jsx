8eac06cd00021412da7b0e91b14c91b540a78561
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-26 17:49:25 +0530

    responsiveness

--- client/src/pages/BlogDetails.jsx
+++ client/src/pages/BlogDetails.jsx
@@ -1,3 +1,136 @@
+// import React from "react";
+// import { useState, useEffect } from "react";
+// import { useParams } from "react-router-dom";
+// import { useNavigate } from "react-router-dom";
+// import { Box, Button, InputLabel, TextField, Typography } from "@mui/material";
+// import axios from "axios";
+
+// const BlogDetails = () => {
+//   const [blog, setBlog] = useState({});
+//   const id = useParams().id;
+//   const navigate = useNavigate();
+//   const [inputs, setInputs] = useState({});
+
+//   const getBlogDetails = async () => {
+//     try {
+//       const { data } = await axios.get(`/api/v1/blog/get-blog/${id}`);
+//       if (data?.success) {
+//         setBlog(data?.blog);
+//         setInputs({
+//           title: data?.blog.title,
+//           description: data?.blog.description,
+//           image: data?.blog.image,
+//         });
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   useEffect(() => {
+//     getBlogDetails();
+//   }, [id]);
+
+//   console.log(blog);
+
+//   const handleSubmit = async (e) => {
+//     e.preventDefault();
+//     console.log(inputs);
+//     try {
+//       const { data } = await axios.put(`/api/v1/blog/update-blog/${id}`, {
+//         title: inputs.title,
+//         description: inputs.description,
+//         image: inputs.image,
+//         user: id,
+//       });
+//       if (data?.success) {
+//         alert("Blog Updated");
+//         navigate("/my-blogs");
+//       }
+//     } catch (error) {
+//       console.log(error);
+//     }
+//   };
+
+//   const handleChange = (e) => {
+//     setInputs((prevState) => ({
+//       ...prevState,
+//       [e.target.name]: e.target.value,
+//     }));
+//   };
+//   return (
+//     <>
+//       <form onSubmit={handleSubmit}>
+//         <Box
+//           width={"50%"}
+//           border={3}
+//           borderRadius={10}
+//           padding={3}
+//           margin="auto"
+//           boxShadow={"10px 10px 20px #ccc"}
+//           display="flex"
+//           flexDirection={"column"}
+//           marginTop="30px"
+//         >
+//           <Typography
+//             variant="h2"
+//             textAlign={"center"}
+//             fontWeight="bold"
+//             padding={3}
+//             color="gray"
+//           >
+//             Update A Post
+//           </Typography>
+//           <InputLabel
+//             sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
+//           >
+//             Title
+//           </InputLabel>
+//           <TextField
+//             name="title"
+//             value={inputs.title}
+//             onChange={handleChange}
+//             margin="normal"
+//             variant="outlined"
+//             required
+//           />{" "}
+//           <InputLabel
+//             sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
+//           >
+//             Description
+//           </InputLabel>
+//           <TextField
+//             name="description"
+//             value={inputs.description}
+//             onChange={handleChange}
+//             margin="normal"
+//             variant="outlined"
+//             required
+//           />
+//           <InputLabel
+//             sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
+//           >
+//             Image URL
+//           </InputLabel>
+//           <TextField
+//             name="image"
+//             value={inputs.image}
+//             onChange={handleChange}
+//             margin="normal"
+//             variant="outlined"
+//             required
+//           />
+//           <Button type="submit" color="warning" variant="contained">
+//             UPDATE
+//           </Button>
+//         </Box>
+//       </form>
+//     </>
+//   );
+// };
+
+// export default BlogDetails;
+
 import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
@@ -31,11 +164,8 @@ const BlogDetails = () => {
     getBlogDetails();
   }, [id]);
 
-  console.log(blog);
-
   const handleSubmit = async (e) => {
     e.preventDefault();
-    console.log(inputs);
     try {
       const { data } = await axios.put(`/api/v1/blog/update-blog/${id}`, {
         title: inputs.title,
@@ -58,11 +188,12 @@ const BlogDetails = () => {
       [e.target.name]: e.target.value,
     }));
   };
+
   return (
     <>
       <form onSubmit={handleSubmit}>
         <Box
-          width={"50%"}
+          width={{ xs: "100%", sm: "80%", md: "60%", lg: "50%" }}
           border={3}
           borderRadius={10}
           padding={3}
@@ -93,7 +224,7 @@ const BlogDetails = () => {
             margin="normal"
             variant="outlined"
             required
-          />{" "}
+          />
           <InputLabel
             sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
           >
