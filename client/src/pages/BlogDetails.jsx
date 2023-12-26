cd295c9cd75dc03b6ad12770c3ee7691b1f0ea77
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-26 12:10:14 +0530

    edit fn

--- client/src/pages/BlogDetails.jsx
+++ client/src/pages/BlogDetails.jsx
@@ -1,11 +1,14 @@
 import React from "react";
 import { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import { Box, Button, InputLabel, TextField, Typography } from "@mui/material";
 import axios from "axios";
 
 const BlogDetails = () => {
   const [blog, setBlog] = useState({});
   const id = useParams().id;
+  const navigate = useNavigate();
   const [inputs, setInputs] = useState({
     title: "",
     description: "",
@@ -55,9 +58,73 @@ const BlogDetails = () => {
     }));
   };
   return (
-    <div>
-      <h1>Hello</h1>
-    </div>
+    <>
+      <form onSubmit={handleSubmit}>
+        <Box
+          width={"50%"}
+          border={3}
+          borderRadius={10}
+          padding={3}
+          margin="auto"
+          boxShadow={"10px 10px 20px #ccc"}
+          display="flex"
+          flexDirection={"column"}
+          marginTop="30px"
+        >
+          <Typography
+            variant="h2"
+            textAlign={"center"}
+            fontWeight="bold"
+            padding={3}
+            color="gray"
+          >
+            Create A Post
+          </Typography>
+          <InputLabel
+            sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
+          >
+            Title
+          </InputLabel>
+          <TextField
+            name="title"
+            value={inputs.title}
+            onChange={handleChange}
+            margin="normal"
+            variant="outlined"
+            required
+          />{" "}
+          <InputLabel
+            sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
+          >
+            Description
+          </InputLabel>
+          <TextField
+            name="description"
+            value={inputs.description}
+            onChange={handleChange}
+            margin="normal"
+            variant="outlined"
+            required
+          />
+          <InputLabel
+            sx={{ mb: 1, mt: 2, fontSize: "24px", fontWeight: "bold" }}
+          >
+            Image URL
+          </InputLabel>
+          <TextField
+            name="image"
+            value={inputs.image}
+            onChange={handleChange}
+            margin="normal"
+            variant="outlined"
+            required
+          />
+          <Button type="submit" color="primary" variant="contained">
+            SUBMIT
+          </Button>
+        </Box>
+      </form>
+    </>
   );
 };
 
