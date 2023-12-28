a86617243dbc66827d8da393e3d5051a9c9f06d5
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-28 18:17:25 +0530

    commit

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -283,13 +283,14 @@ export default function BlogCard({
                   viewBox="0 0 24 24"
                   fill="none"
                   stroke="currentColor"
-                  strokeWidth="2"
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  className="feather feather-user"
+                  stroke-width="2"
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  class="lucide lucide-circle-user-round"
                 >
-                  <path d="M19 21v-2a4 4 0 0 0-4-4H9a4 4 0 0 0-4 4v2" />
-                  <circle cx="12" cy="7" r="4" />
+                  <path d="M18 20a6 6 0 0 0-12 0" />
+                  <circle cx="12" cy="10" r="4" />
+                  <circle cx="12" cy="12" r="10" />
                 </svg>
 
                 <span style={{ marginLeft: "5px" }}>{username}</span>
