b6ca65c37121f4214c8b74784e68c8e1e37ad756
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2023-12-28 12:45:23 +0530

    commit

--- client/src/components/BlogCard.jsx
+++ client/src/components/BlogCard.jsx
@@ -1,14 +1,4 @@
 import * as React from "react";
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardMedia from "@mui/material/CardMedia";
-import CardContent from "@mui/material/CardContent";
-import Avatar from "@mui/material/Avatar";
-import Typography from "@mui/material/Typography";
-import { red } from "@mui/material/colors";
-import ModeEditIcon from "@mui/icons-material/ModeEdit";
-import DeleteIcon from "@mui/icons-material/Delete";
-import { Box, IconButton } from "@mui/material";
 import { useNavigate } from "react-router-dom";
 // import toast from "react-hot-toast";
 import axios from "axios";
