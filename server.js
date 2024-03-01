cf9a7150bc9a43d906508a6cbb4b2928cf9ab55b
Vatsal Agarwal <bindalvatsaljpr@gmail.com>
2024-03-01 21:40:46 +0530

    changes in port no.

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ app.use("/api/v1/blog", blogRoutes);
 
 
 // Port
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 5000;
 //listen
 app.listen(PORT, () => {
     console.log(
